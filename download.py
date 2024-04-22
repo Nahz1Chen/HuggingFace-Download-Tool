ce350697bd35ffba59d22db1f93112d8e07404c0
Nahz1_Chen <nahz_chen@foxmail.com>
2024-04-22 12:58:05 +0800

    repo_type include

--- download.py
+++ download.py
@@ -7,9 +7,9 @@ from huggingface_hub import snapshot_download
 # 如果需要代理的话，去掉此部分注释加入端口
 os.environ["https_proxy"] = "127.0.0.1:7890" # 代理设置
 # os.environ["http_proxy"] = "http://xxxxxxx:xxxx" # 代理设置
-repo_id = "lllyasviel/ControlNet-v1-1" # 模型在huggingface上的名称
+repo_id = "yangtao9009/DIV2K" # 模型在huggingface上的名称
 cache_dir = "./cache/"
-local_dir = "D:/ControlNet-v1-1"
+local_dir = "D:/DIV2K"
 # 指定要创建的目录路径
 
 local_dir_use_symlinks = False # 本地模型使用文件保存，而非blob形式保存
@@ -27,6 +27,7 @@ if not os.path.exists(local_dir):
 #
 snapshot_download(
  repo_id=repo_id,
+ repo_type='dataset',#Accepted repo types are: [None, 'model', 'dataset', 'space']
  cache_dir=cache_dir,
  local_dir=local_dir,
  local_dir_use_symlinks=local_dir_use_symlinks,
